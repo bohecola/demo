d035e047a8a3518df5e9fc1ddee143f824a30223
bohecola <bohecola@outlook.com>
2021-06-22 18:13:56 +0800

    ipcMain

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var electron = require('electron')
+const { ipcMain } = require('electron')
 
 var app = electron.app
 var globalShortcut = electron.globalShortcut
@@ -17,17 +18,35 @@ app.on('ready', () => {
     }
   })
 
+  // 全局快捷键
   globalShortcut.register('ctrl+e', () => {
     mainWindow.loadURL('https://jspang.com')
   })
-
   let isRegister = globalShortcut.isRegistered('ctrl+e') ? 'Register Success' : 'Register fail'
   console.log('------>' + isRegister)
 
-  mainWindow.webContents.openDevTools();
-  require('./main/menu.js')
-  mainWindow.loadFile('demo7.html')
+  // ipcMain 监听异步消息
+  ipcMain.on('asynchronous-message', (event, arg) => {
+    console.log('From render-message async ->', arg) // prints 'ping'
+    // 回复渲染进程异步消息 'pong'
+    event.reply('asynchronous-reply', 'pong')
+  })
+  
+  // ipcMain 监听同步消息
+  ipcMain.on('synchronous-message', (event, arg) => {
+    console.log('From render-message sync ->', arg) // prints 'ping'
+    // 回复渲染进程同步消息 'pong'
+    event.returnValue = 'pong'
+  })
 
+  // 调试工具
+  mainWindow.webContents.openDevTools();
+
+  // 菜单
+  require('./main/menu.js')
+
+  // 窗口加载视图文件
+  mainWindow.loadFile('demo8.html')
   // BrowserView
   // var BrowserView = electron.BrowserView
   // var view = new BrowserView()
